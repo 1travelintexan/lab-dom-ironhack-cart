8267419517179d77f5f85305400c9ff06b1f0490
Joshua George <horheyinc8@gmail.com>
2021-04-01 22:05:24 +0200

    updated cart

--- js/index.js
+++ js/index.js
@@ -1,44 +1,37 @@
 // ITERATION 1
 
 function updateSubtotal(product) {
- 
+
    const price = product.querySelector('.price span')
-  const quantity = product.querySelector('.quantity input')
-   let priceValue = price.innerText
-   let quantityValue = quantity.value
-  
-  let subtotal = priceValue * quantityValue
-  
-  const subtotalElement = product.querySelector('.subtotal span')
-  subtotalElement.innerText = subtotal
+  const quantityDom = product.querySelector('.quantity input')
+  let priceDom = Number(price.innerText)
+  let quantity = Number(quantityDom.value)
+
+  let subtotal = priceDom * quantity
+
+  let subtotalDom = product.querySelector('.subtotal span')
+    subtotalDom.innerHTML = subtotal
 
   return subtotal
 }
 
 function calculateAll() {
-  // ITERATION 2
   
-const sub1 = document.querySelectorAll('.product')
-let grandTotal = 0
+  const AllProducts = document.querySelectorAll('.product ')
+  
+  total = 0
+  AllProducts.forEach((element) => {
+   total = total + updateSubtotal(element)
+  })
+   
+  let totalValue = document.querySelector('#total-value span')
+   totalValue.innerHTML = total
 
-sub1.forEach((element) => {
-  let total1 = updateSubtotal(element)
-   grandTotal = total1 + grandTotal    
-})
-
-return grandTotal
-
-
-
+}
 
   // ITERATION 3
 
-  const cal = document.querySelector('#calculate')
-  cal.addEventListener('click' () => {
-    let finalPrice = document.querySelector('#total-value span')
-    finalPrice.innerText = grandTotal
-  })
-}
+
 
 // ITERATION 4
 
